df5bc991435c2cf59a03436ecb439d2fab397f94
liviarita <37383858+liviarita@users.noreply.github.com>
2021-06-10 20:37:52 -0300

    creating the controllers

--- controllers/account-controller.js
+++ controllers/account-controller.js
@@ -21,3 +21,88 @@ exports.listAllClients = async (req, res) => {
         res.json(documents);
     });
 };
+
+exports.listAllProviders = async (req, res) => {
+    connectionInstance().db.collection(COLLECTIONS.providers).find({}).toArray((error, documents) => {
+        if(error){
+            return res.status(400).json('error');
+        }
+        res.json(documents);
+    });
+};
+
+exports.listAccountById = async (req, res) => {
+    const id = req.params.id;
+    const filter = { _id: mongoose.Types.ObjectId(id)};
+    const callback = (error, documents) => {
+        if(error) {
+            return res.status(400).json(error);
+        }
+        res.json(documents);
+    };
+    connectionInstance().db.collection(COLLECTIONS.accounts).findOne(filter, callback);
+};
+
+exports.createAccount = async (req, res) => {
+    const data = req.body;
+    const options = {};
+    const callback = (error, documentCreated) => {
+        if(error){
+            return res.status(400).json(error);
+        }
+        res.json(documentCreated);
+    };
+    connectionInstance().db.collection(COLLECTIONS.accounts).insertOne(data, options, callback);
+};
+
+exports.createClient = async (req, res) => {
+    const data = req.body;
+    const options = {};
+    const callback = (error, documentCreated) => {
+        if(error){
+            return res.status(400).json(error);
+        }
+        res.json(documentCreated);
+    };
+    connectionInstance().db.collection(COLLECTIONS.clients).insertOne(data, options, callback);
+};
+
+exports.createProvider = async (req, res) => {
+    const data = req.body;
+    const options = {};
+    const callback = (error, documentCreated) => {
+        if(error){
+            return res.status(400).json(error);
+        }
+        res.json(documentCreated);
+    };
+    connectionInstance().db.collection(COLLECTIONS.providers).insertOne(data, options, callback);
+};
+
+exports.updateAccount = async (req, res) => {
+    const id = req.query.id;
+    const data = {$set: req.body};
+    const filter = { _id: mongoose.Types.ObjectId(id)};
+    const options = {};
+    const callback = (error, documentUpdated) => {
+        if(error){
+            return res.status(400).json(error);
+        }
+        res.json(documentUpdated);
+    };
+    connectionInstance().db.collection(COLLECTIONS.accounts).updateOne(filter, data, options, callback);
+};
+
+exports.deleteAcoount = async (req, res) => {
+    const id = req.query.id;
+    const options = {};
+    const filter = { _id: mongoose.Types.ObjectId(id)};
+    const callback = (error, documentDeleted) => {
+        if(error){
+            return res.status(400).json(error);
+        }
+        res.json(documentDeleted);
+    };
+    connectionInstance().db.collection(COLLECTIONS.accounts).deleteOne(filter, options, callback);
+};
+
